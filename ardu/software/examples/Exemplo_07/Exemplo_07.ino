980f8dd478d2072b2d872bacd83beeab6bc03135
eduardojorge <emjorge1974@gmail.com>
2025-02-03 14:55:41 -0300

    Software Robô Ardu

---
+++ ardu/software/examples/Exemplo_07/Exemplo_07.ino
@@ -0,0 +1,18 @@
+#include <Robotica_Cognitiva.h>
+
+long tempoInicial = millis();
+long intervalo = 6000;
+
+void loop(){
+ long tempoAtual = millis();
+ long tempoDiferenca = tempoAtual - tempoInicial;
+ if(tempoDiferenca>intervalo){
+  Sentimento.Feliz();
+  Acao.Toca_Buzzer(Feliz_1);
+  tempoInicial=tempoAtual;
+ }else{
+
+  Acao.Apaga_Tudo();
+  delay(2000);
+ }
+}
---
+++ ardu/software/library/Robotica_Cognitiva/Sentimentos.h
@@ -0,0 +1,19 @@
+#ifndef Sentimentos_h
+
+#define Sentimentos_h
+
+
+#include "Arduino.h"
+#include "Acoes.h"
+
+class Sentimentos{
+    private:
+        Acoes Acao = Acoes(13);
+        
+    public:
+      Sentimentos(int x);
+      void Feliz();
+      void Triste();
+      void Raiva();
+};
+#endif
